b2daa9240ac0da893626853b701c7f29fe30b9b4
Artyom Revinov <artyomrevinov@gmail.com>
2019-02-16 11:18:37 +0600

    Added Specialties Map Impl

---
+++ pet-clinic-data/src/main/java/springframework/petclinic/services/SpecialtiesService.java
@@ -0,0 +1,9 @@
+package springframework.petclinic.services;
+
+import springframework.petclinic.model.Specialty;
+
+/**
+ * Created by revin on 16.02.2019.
+ */
+public interface SpecialtiesService extends CrudService<Specialty, Long>{
+}
