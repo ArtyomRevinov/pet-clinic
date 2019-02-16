957d8c6d505b7174014118d365aa83725510f980
Artyom Revinov <artyomrevinov@gmail.com>
2019-02-16 10:30:30 +0600

    added PetType service

---
+++ pet-clinic-data/src/main/java/springframework/petclinic/services/PetTypeService.java
@@ -0,0 +1,9 @@
+package springframework.petclinic.services;
+
+import springframework.petclinic.model.PetType;
+
+/**
+ * Created by revin on 16.02.2019.
+ */
+public interface PetTypeService extends CrudService<PetType, Long>{
+}
